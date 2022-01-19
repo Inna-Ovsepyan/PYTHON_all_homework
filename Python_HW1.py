b6584a37c66c652f77d8f3b5f5a1b20358f388b0
Inna-Ovsepyan <97498514+Inna-Ovsepyan@users.noreply.github.com>
2022-01-19 19:58:50 +0700

    file .py

---
+++ Python_HW1.py
@@ -0,0 +1,73 @@
+a= ("hello")
+print(type(a)) #str
+
+b= 55
+print(type(b)) #int
+
+c= 25.6
+print(type(c)) #float
+
+d= bytes(11)
+print(type(d)) #bytes
+
+e= ["apple", "cherry", "banana", "pear"]
+print(type(e)) #list
+
+h= ("sofa", "chair", "table")
+print(type(h)) #tuple
+
+s = {"rose", "tulip", "iris", "peony"}
+print(type(s)) #set
+
+x= frozenset("fruit") #frosenset
+print(type(x))
+
+y={1:"a", 2:"b", 3:"c"}
+print(type(y)) #dict
+
+i=("home")
+j=("work")
+print(i+j)
+
+p= 1
+t=("metr")
+print(p, str(t))
+
+k= 33
+l= ("cows")
+print(l+ str(k))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
